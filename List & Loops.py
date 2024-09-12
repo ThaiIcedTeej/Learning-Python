44de7aec1d53181387f1b9caa472f37af18e8378
ThaiIcedTeej <tjtrimas@gmail.com>
2024-09-12 04:31:02 -0500

    Add files via upload

---
+++ List & Loops.py
@@ -0,0 +1,17 @@
+# buying = ["apple", "banana"]
+#
+# def checkout(fruits):
+#     cost = 0
+#
+#     for fruit in range(0, len(fruits)):
+#         if fruits[fruit] == "apple":
+#             cost += 3
+#         elif fruits[fruit] == "banana":
+#             cost += 2
+#         elif fruits[fruit] == "strawberry":
+#             cost += 5
+#         elif fruits[fruit] == "pineapple":
+#             cost += 1
+#     return cost
+#
+# print(checkout(buying))
